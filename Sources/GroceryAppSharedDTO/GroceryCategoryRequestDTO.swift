43d3b14a3964ed9f83535a83e04e3af419134e1e
Juan Collin <juanlucas09@hotmail.com>
2024-06-24 18:51:01 -0300

    🚧 Updated the GroceryCategory DTO files

--- Sources/GroceryAppSharedDTO/GroceryCategoryRequestDTO.swift
+++ Sources/GroceryAppSharedDTO/GroceryCategoryRequestDTO.swift
@@ -1,8 +1,12 @@
-//
-//  File.swift
-//  
-//
-//  Created by Juan Collin on 24/06/24.
-//
-
 import Foundation
+
+public struct GroceryCategoryRequestDTO: Codable {
+    
+    public let title: String
+    public let colorCode: String
+    
+    public init(title: String, colorCode: String) {
+        self.title = title
+        self.colorCode = colorCode
+    }
+}
--- Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
+++ Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
@@ -1,8 +1,14 @@
-//
-//  File.swift
-//  
-//
-//  Created by Juan Collin on 24/06/24.
-//
-
 import Foundation
+
+public struct GroceryCategoryResponseDTO: Codable {
+    
+    public let id: UUID
+    public let title: String
+    public let colorCode: String
+    
+    public init(id: UUID, title: String, colorCode: String) {
+        self.id = id
+        self.title = title
+        self.colorCode = colorCode
+    }
+}
